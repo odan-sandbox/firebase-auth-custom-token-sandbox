64673e160e54868c7cc855448da9d13c8d78b185
odan <odan3240@gmail.com>
2020-03-24 06:44:44 +0900

    add

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,34 @@
-export function add(x: number, y: number): number {
-  return x + y;
+import admin from "firebase-admin";
+import firebase from "firebase";
+
+import serviceAccount from "../service-account-key.json";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC1Wkk7d-_LH4pKujWg2g5MTjbTdSIlfQk",
+  projectId: "odan-firebase-auth-sandbox"
+});
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: serviceAccount.project_id,
+    clientEmail: serviceAccount.client_email,
+    privateKey: serviceAccount.private_key
+  })
+});
+
+async function main(): Promise<void> {
+  const uid = "test";
+  const customToken = await admin.auth().createCustomToken(uid);
+
+  console.log(
+    (await firebase.auth().signInWithCustomToken(customToken)).user?.uid
+  );
+  console.log(
+    (await firebase.auth().signInWithCustomToken(customToken)).user?.uid
+  );
+  console.log(
+    (await firebase.auth().signInWithCustomToken(customToken)).user?.uid
+  );
 }
 
-console.log("poyo");
+main().catch(e => console.error(e));
